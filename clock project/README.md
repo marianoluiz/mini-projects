d880c97b50b9744c42ac46b58940d006793f5ac4
marianoluiz <kmarianu@gmail.com>
2025-03-28 01:08:19 +0800

    initial commit

---
+++ clock project/README.md
@@ -0,0 +1,8 @@
+# Project Details
+
+![Morning image](./img/sample-img.png "Day theme sample")
+![Night image](./img/sample-img2.png "Night theme sample")
+
+- This is a simple clock project where the theme can be toggled [ day / night ].
+- Language used: `HTML`, `CSS`, `JavaScript`
+
--- clock project/script.js
+++ clock project/script.js
@@ -1,24 +1,76 @@
-
+// update time function
 const timer = document.querySelector(".time");
 
-const updateTime =() => {
+// toggle variables
+const boolToggleInput = document.querySelector(".toggle");
+const slider = document.querySelector(".slider");
+const body = document.querySelector("body");
+const timeContainer = document.querySelector(".time__container");
+const timeText = document.querySelector(".time");
+
+// onload
+window.onload = () => {
+  setInterval(updateTime, 1000);
+
+  // if it is am, on the toggle (morning)
+  // if its pm , off the toggle (night)
+  // return if am or pm
+  const meridiemIndicator = updateTime();
+
+  if (meridiemIndicator === "AM") {
+    boolToggleInput.checked = false;
+  } else {
+    boolToggleInput.checked = true;
+  }
+
+  toggleThemeFunc();
+};
+
+const updateTime = () => {
   const date = new Date();
   let hrs = date.getHours();
   const mins = date.getMinutes();
   const secs = date.getSeconds();
-  let meridiemIndicator = '';
+  let meridiemIndicator = "";
 
-  if (hrs > 12 ) {
-    meridiemIndicator = 'PM'
+  if (hrs > 12) {
+    meridiemIndicator = "PM";
     hrs = hrs - 12;
   } else {
     meridiemIndicator = "AM";
   }
 
   timer.textContent = `${hrs} : ${mins} : ${secs} ${meridiemIndicator}`;
-}
 
-window.onload = () => {
-  setInterval(updateTime, 1000);
-  updateTime();
-}
+  return meridiemIndicator;
+};
+
+// check theme based on toggle func
+const toggleThemeFunc = () => {
+  if (boolToggleInput.checked === true) {
+    // move the slider
+    slider.classList.add("toggled");
+
+    //change the bg
+    body.classList.add("night");
+
+    // time container
+    timeContainer.classList.add("night");
+
+    // time
+    timeText.classList.add("night");
+  } else {
+    slider.classList.remove("toggled");
+
+    //change the bg
+    body.classList.remove("night");
+
+    // time container
+    timeContainer.classList.remove("night");
+
+    // time
+    timeText.classList.remove("night");
+  }
+};
+
+boolToggleInput.addEventListener("click", toggleThemeFunc);
